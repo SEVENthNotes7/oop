4f8bbbb4a5e86bfec8b12d982b1984086890d198
CedAlegsao <cedrickcuencaalegsao@gmail.com>
2024-05-11 15:01:24 +0800

    products

---
+++ medterm_act/products/index.php
@@ -0,0 +1,76 @@
+<?php
+  $host = "localhost";
+  $username = "root";
+  $password = "";
+  $db = "productdb";
+
+  $conn = new mysqli($host, $username, $password, $db);
+
+  if ($conn->connect_error) {
+    dei("Connection failes" . $conn->connect_error);
+  }
+
+  $sql = "select * from tbl_product";
+  $rs = $conn->query($sql);
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <div class="container">
+    <div class="form-container">
+      <!-- form -->
+      <form action="saveimage.php" method="POST" enctype="multipart/form-data">
+        <h3>Add Product</h3>
+        <span>Product Name: </span>
+        <input type="text" name="product_name"><br>
+        <span>Price:</span>
+        <input type="number" name="product_price"><br>
+        <span>Quantity: </span>
+        <input type="number" name="product_quantity"><br>
+        <span>Chose Image: </span>
+        <input type="file" name="product_image"><br>
+        <input type="submit" name="submit" id="btnsubmit" value="Save Product">
+      </form>
+    </div>
+    <div class="product-table">
+      <table>
+        <thead>
+          <tr>
+            <th>Product Image</th>
+            <th>Product Name</th>
+            <th>Price</th>
+            <th>Quantity</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          <?php
+            if ($rs->num_rows > 0) {
+            while ($row = $rs->fetch_assoc()) {
+                echo "
+                <tr>
+                    <td>" . $row["id"] . "</td>
+                    <td>" . $row["fname"] . "</td>
+                    <td>" . $row["lname"] . "</td>
+                    <td>" . $row["pnum"] . "</td>
+                    <td><a href='delete.php?id=".$row["id"]."'>Delete</a></td>
+                </tr>";
+              }
+            }
+            $conn->close();
+          ?>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</body>
+
+</html>
---
+++ medterm_act/products/saveimage.php
@@ -0,0 +1,47 @@
+<?php
+$host = "localhost";
+$username = "root";
+$password = "";
+$db = "productdb";
+
+$conn = new mysqli($host, $username, $password, $db);
+
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$target_dir = "img/";
+$target_file = $target_dir . basename($_FILES["product_image"]["name"]);
+
+if (!empty($_POST["submit"])) {
+  $product_name = $_POST["product_name"];
+  $price = $_POST["product_price"];
+  $quant = $_POST["product_quantity"];
+
+  if (move_uploaded_file($_FILES["product_image"]["tmp_name"], $target_file)) {
+      echo "The file " . htmlspecialchars(basename($_FILES["product_image"]["name"])) . " has been uploaded successfully.<br>";
+
+      $imageName = "img/" . htmlspecialchars(basename($_FILES["product_image"]["name"]));
+
+      $sql = $conn->prepare("INSERT INTO tbl_product (product_name, price, quantity, imageloc) VALUES (?, ?, ?, ?)");
+      
+      $sql->bind_param("sdis", $product_name, $price, $quant, $imageName);
+      $sql->execute();
+
+      // Check if the insertion was successful
+      if ($sql->affected_rows > 0) {
+          echo "Product details inserted into database successfully.<br>";
+      } else {
+          echo "Error inserting product details into database.<br>";
+      }
+
+      $sql->close();
+  } else {
+      echo "Failed to upload image. <a href='index.php'>Go back</a>";
+  }
+} else {
+  echo "No data submitted. <a href='index.php'>Go back</a>";
+}
+
+$conn->close();
+?>
